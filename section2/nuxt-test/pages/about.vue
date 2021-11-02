dfdc69b0bba884efdb9ca884ec88b8d3fe557c16
aokitashipro <webmocha2@gmail.com>
2021-11-03 05:11:46 +0900

    add assets

--- section2/nuxt-test/pages/about.vue
+++ section2/nuxt-test/pages/about.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     about<br>
-    {{ now }}
+    {{ now }}<br>
+    <img src="~/assets/images/nuxt-course.jpg" width="300"><br>
+    <div class="red-b">assets/cssのテスト</div>
   </div>
 </template>
 
